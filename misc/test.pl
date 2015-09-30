e12133018a191b73f99308da70e751072cf5836a
Yulai <punka86@mail.ru>
2015-09-30 15:24:22 +0300

    remove misc

--- misc/test.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-use DBI;
-
-@drivers = DBI->available_drivers;
-print "------------- drivers -----------\n";
-for $i (@drivers) {
-  print "$i\n";
-}
-print "---------------------------\n";
